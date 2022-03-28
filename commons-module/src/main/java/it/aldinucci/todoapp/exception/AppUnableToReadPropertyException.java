4fbb28ee7e477b3018d78f4f61fbb26bd69916b5
Piero <piero.aldinucci@gmail.com>
2022-03-28 13:39:18 +0200

    remove useless code

--- commons-module/src/main/java/it/aldinucci/todoapp/exception/AppUnableToReadPropertyException.java
+++ commons-module/src/main/java/it/aldinucci/todoapp/exception/AppUnableToReadPropertyException.java
@@ -7,10 +7,6 @@ public class AppUnableToReadPropertyException extends RuntimeException{
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	public AppUnableToReadPropertyException() {
-		super();
-	}
-	
 	public AppUnableToReadPropertyException(Throwable ex) {
 		super(ex);
 	}
