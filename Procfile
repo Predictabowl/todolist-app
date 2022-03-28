64e342067ae401d3356e8f7e08692d8d6749de43
Piero <piero.aldinucci@gmail.com>
2022-03-28 18:10:25 +0200

    Cleaned pom. Added procfile for Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar configuration-module/target/configuration-module-0.0.1-SNAPSHOT.jar
--- commons-module/src/main/java/it/aldinucci/todoapp/config/ApplicationPropertyNames.java
+++ commons-module/src/main/java/it/aldinucci/todoapp/config/ApplicationPropertyNames.java
@@ -8,7 +8,6 @@ public class ApplicationPropertyNames {
 	public static final String VERIFICATION_EMAIL_PASSWORD = "todolist.verification.email.password";
 	public static final String VERIFICATION_EMAIL_HOST= "todolist.verification.email.host";
 	public static final String VERIFICATION_EMAIL_PORT= "todolist.verification.email.port";
-	public static final String VERIFICATION_EMAIL_SENDER= "todolist.verification.email.sender";
 	public static final String VERIFICATION_EMAIL_PROTOCOL= "todolist.verification.email.protocol";
 	public static final String VERIFICATION_EMAIL_AUTH= "todolist.verification.email.auth";
 	public static final String VERIFICATION_EMAIL_SSL= "todolist.verification.email.ssl";
