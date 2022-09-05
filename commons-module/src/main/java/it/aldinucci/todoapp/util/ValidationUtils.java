6f4a6f53fa9ef1d7c1cd3f6b2fae86c12a3e17de
Piero <piero.aldinucci@gmail.com>
2022-09-05 17:38:10 +0200

    Updated UUID regex

--- commons-module/src/main/java/it/aldinucci/todoapp/util/ValidationUtils.java
+++ commons-module/src/main/java/it/aldinucci/todoapp/util/ValidationUtils.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 public class ValidationUtils {
 
 	private static final Pattern UUID_REGEX =
-			Pattern.compile("^[{]?[0-9a-fA-F]{8}-([0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12}[}]?$");
+			Pattern.compile("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$");
 	
 	private ValidationUtils() {
 	}
--- commons-module/src/test/java/it/aldinucci/todoapp/util/ValidationUtilsTest.java
+++ commons-module/src/test/java/it/aldinucci/todoapp/util/ValidationUtilsTest.java
@@ -4,6 +4,7 @@ package it.aldinucci.todoapp.util;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.UUID;
+import java.util.stream.IntStream;
 
 import org.junit.jupiter.api.Test;
 
@@ -25,9 +26,8 @@ class ValidationUtilsTest {
 	
 	@Test
 	void test_Valid() {
-		String notUuid = UUID.randomUUID().toString();
-		
-		assertThat(ValidationUtils.isValidUUID(notUuid)).isTrue();
+		IntStream.range(0, 1000).mapToObj(i -> UUID.randomUUID().toString())
+			.forEach(u -> assertThat(ValidationUtils.isValidUUID(u)).isTrue()); 
 	}
 	
 	@Test
